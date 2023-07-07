59033a1b5dea7d395eaa6106c12554b592175d3a
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-07-07 15:40:59 -0600

    Reassign two variables and add puts statements

--- variables.rb
+++ variables.rb
@@ -6,3 +6,11 @@ account_balance = 2300.08
 item_price = 5.46
 at_home = true
 in_kitchen = false
+name = "Moe"
+pet = "Clara"
+
+puts "You are #{age} years old!"
+puts "Your new account balance is #{account_balance - item_price}."
+puts "Your pet, #{pet}, is #{pet_age} years old."
+if at_home = true then puts "Welcome home, " + name + "."
+end
